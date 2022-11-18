1fa71f4d89e6b6fd2f93f9d40196c9f9813f0706
Скоробогатый Евгений <evskorobogatij@gmail.com>
2022-11-18 15:34:12 +0300

    Заголовок приложения

--- src/modules/Application/containers/ApplicationHeader.tsx
+++ src/modules/Application/containers/ApplicationHeader.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import { useGetApplicationQuery } from '../applicationService'
 
 const ApplicationHeader: React.FC = () => {
   const { data } = useGetApplicationQuery(null)
+
+  useEffect(() => {
+    if (data && data.nameOfAppTitle) {
+      document.title = data.nameOfAppTitle
+    }
+  }, [data])
 
   return (
     <Box component="div">
