ff7232c3c3aa4672dd9d123332b4986913021931
Скоробогатый Евгений <evskorobogatij@gmail.com>
2022-12-05 09:00:08 +0300

    Общая статистика

--- src/modules/Statistic/pages/StatisticCommonPage.tsx
+++ src/modules/Statistic/pages/StatisticCommonPage.tsx
@@ -1,4 +1,5 @@
 import { Grid, Typography } from '@mui/material'
+import DailyStatContainer from 'modules/Dashboard/containers/DailyStatContainer'
 import { useTranslation } from 'react-i18next'
 
 export const StatisticCommonPage = () => {
@@ -10,6 +11,12 @@ export const StatisticCommonPage = () => {
           <Typography variant="h5">
             {t('Statistic')}. {t('statistic.common')}
           </Typography>
+        </Grid>
+        <Grid item lg={12} xs={12} md={12} sm={12}>
+          <Typography variant="h6" color="text.secondary" sx={{ mb: 1, mt: 3 }}>
+            {t('Daily')}
+          </Typography>
+          <DailyStatContainer />
         </Grid>
       </Grid>
     </>
