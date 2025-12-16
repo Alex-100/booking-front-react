f1f8d1cc5681c04d7686c16034c98ad9d90b530f
Mikhael Tatischev <tatischevm@gmai.com>
2025-12-16 20:29:20 +0300

    Update cookie after logout

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -38,6 +38,8 @@ export const useAuth = () => {
     document.cookie = "accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     document.cookie = "refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "hospital=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "department=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
   }
 
   return {
